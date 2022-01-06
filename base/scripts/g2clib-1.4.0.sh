cb0cd9a2acadc8fbaff1cc0608e68f384dd09bdf
James McDuffie <James.McDuffie@jpl.nasa.gov>
2022-01-06 11:39:04 -0800

    Fix g2clib compilation issues, Set hostname explicitly to allow certain unit tests to pass. Copy in updated leap seconds file.

--- base/scripts/g2clib-1.4.0.sh
+++ base/scripts/g2clib-1.4.0.sh
@@ -4,12 +4,17 @@
 #
 tar zxvf $SOURCES_PATH/g2clib-1.4.0.tar.gz
 cd g2clib-1.4.0/src
+
+# Remove makefile line that removes object files or else some 
+# source code files will not be included in library
+sed -i '/rm -f/d' makefile
+
 export CC=$GCC_PATH/bin/gcc \
 export CFLAGS="-m64 -fPIC -pthread -mcmodel=large -Wall -Wno-unused-variable \
         -DUSE_PNG -DUSE_JPEG2000 \
         -Wmissing-prototypes -Wmissing-declarations -m128bit-long-double -mlarge-data-threshold=90000000 \
         -I$INSTALL_PREFIX/jasper-1.900.1/include"
-make CFLAGS="$CFLAGS" CC="$CC" -j $NUM_COMPILE_JOBS
+make CFLAGS="$CFLAGS" CC="$CC"
 PREFIX=$INSTALL_PREFIX/g2clib-1.4.0
 mkdir -p $PREFIX/{lib,include}
 install -p -m0644 libgrib2c.a $PREFIX/lib
