c40df805e3570e9aa97bbc202dac903090d946f0
James McDuffie <James.McDuffie@jpl.nasa.gov>
2022-03-07 14:31:16 -0800

    Change run_l1a_test.sh to not overwrite PGE_IN_DIR, PGE_OUT_DIR and PGE_STATIC_DIR if supplied as environment variables.

--- spss/test/common/setup_env.sh
+++ spss/test/common/setup_env.sh
@@ -14,8 +14,23 @@ fi
 export READLINK_BIN
 
 # Map subdirectories of the scripts' directory as in and out paths
-export PGE_IN_DIR=$($READLINK_BIN -f $base_dir/in)
-export PGE_OUT_DIR=$($READLINK_BIN -f $base_dir/out)
+if [ -z "$PGE_IN_DIR" ]; then
+    export PGE_IN_DIR=$($READLINK_BIN -f $base_dir/in)
+fi
+
+if [ ! -e "$PGE_IN_DIR" ]; then
+    echo "Can not find PGE_IN_DIR: $PGE_IN_DIR"
+    exit 1
+fi
+
+if [ -z "$PGE_OUT_DIR" ]; then
+    export PGE_OUT_DIR=$($READLINK_BIN -f $base_dir/out)
+fi
+
+if [ ! -e "$PGE_OUT_DIR" ]; then
+    echo "Can not find PGE_OUT_DIR: $PGE_OUT_DIR"
+    exit 1
+fi
 
 # Include variables from .env file so we have $DOCKER_TAG available
 env_file=$($READLINK_BIN -f $base_dir/../../../.env)
@@ -23,12 +38,10 @@ env_file=$($READLINK_BIN -f $base_dir/../../../.env)
 
 # Find static files location
 lcl_static_dir=$($READLINK_BIN -f $base_dir/../../../static)
-if [ ! -z "$SIPS_STATIC_DIR" ]; then
-    PGE_STATIC_DIR=$SIPS_STATIC_DIR
-elif [ -e "$lcl_static_dir" ]; then
+if [ -z "$PGE_STATIC_DIR" ] && [ -e "$lcl_static_dir" ]; then
     PGE_STATIC_DIR=$lcl_static_dir
 else
-    echo "Could not find static files directory from either a \$SIPS_STATIC_DIR environment variable or at the path: $lcl_static_dir"
+    echo "Could not find static files directory from either a \$PGE_STATIC_DIR environment variable or at the path: $lcl_static_dir"
     exit 1
 fi
 export PGE_STATIC_DIR
