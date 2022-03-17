5ca996ac8270210308315f5c5ce992ba24b7cccd
James McDuffie <James.McDuffie@jpl.nasa.gov>
2022-03-17 11:18:21 -0700

    Add L1B tester that uses a temporary directory

--- spss/test/l1b/run_l1b_test_local.sh
+++ spss/test/l1b/run_l1b_test_local.sh
@@ -4,10 +4,10 @@
 source $(dirname $0)/../common/setup_env.sh $(dirname $0)
 
 # Copy input files from SPSS repo
-acc_case_in_dir=$($READLINK_BIN -f $(dirname $0)/../../src/src/sips_pge/l1b_atms_snpp/spdc_nominal/in/l1a/)
+acc_case_in_dir=$($READLINK_BIN -f $(dirname $0)/../../src/src/sips_pge/l1b_atms_snpp/acctest/in/l1a/)
 
 # Copy the input files referenced by the test XML file1
-for src_fn in $acc_case_in_dir; do
+for src_fn in $acc_case_in_dir/*.nc; do
     dest_fn=${PGE_IN_DIR}/$(basename $src_fn)
     if [ ! -e "$dest_fn" ]; then
         cp -a $src_fn $dest_fn
