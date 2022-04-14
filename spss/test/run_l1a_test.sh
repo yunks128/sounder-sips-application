932dace1c217ee8dedbd8abb06dcc9f105221243
James McDuffie <James.McDuffie@jpl.nasa.gov>
2022-04-14 15:18:24 -0700

    Change test scripts to utilize CWL tool instead of directly calling Docker

--- spss/test/run_l1a_test.sh
+++ spss/test/run_l1a_test.sh
@@ -6,14 +6,20 @@ source $(dirname $0)/common/setup_env.sh $(dirname $0)
 # Copy input L0 files from SPSS source tree
 . $(dirname $0)/common/copy_l1a_input.sh
 
-docker run --rm \
-    -v ${PGE_IN_DIR}:${PGE_IN_DIR} \
-    -v ${PGE_OUT_DIR}:${PGE_OUT_DIR} \
-    -v ${PGE_STATIC_DIR}:${temp_dir}/static \
-    unity-sds/sounder_sips_l1a_pge:${DOCKER_TAG} \
-    -p input_path ${PGE_IN_DIR} \
-    -p output_path ${PGE_OUT_DIR} \
-    -p data_static_path ${temp_dir}/static $
-    $*
+cwl_path=$(realpath "$(dirname $0)"/../cwl)
+
+if [ -z "$(which cwltool)" ]; then
+    echo "cwltool was not found and required."
+    exit 1
+fi
+
+cwltool \
+    --outdir ${PGE_OUT_DIR} \
+    ${cwl_path}/l1a_package.cwl \
+    --input_dir ${PGE_IN_DIR} \
+    --static_dir ${PGE_STATIC_DIR} \
+    --start_datetime $(grep start_datetime ${cwl_path}/l1a_package.yml | awk '{print $2}' | sed 's/\"//g') \
+    --end_datetime $(grep end_datetime ${cwl_path}/l1a_package.yml | awk '{print $2}' | sed 's/\"//g') \
+    $* |& tee ${PGE_OUT_DIR}/cwltool.log
 
 echo "Output available at: $PGE_OUT_DIR"
--- spss/test/run_l1b_test.sh
+++ spss/test/run_l1b_test.sh
@@ -6,14 +6,17 @@ source $(dirname $0)/common/setup_env.sh $(dirname $0)
 # Copy input L1A files from SPSS source tree
 . $(dirname $0)/common/copy_l1b_input.sh
 
-docker run --rm \
-    -v ${PGE_IN_DIR}:${PGE_IN_DIR} \
-    -v ${PGE_OUT_DIR}:${PGE_OUT_DIR} \
-    -v ${PGE_STATIC_DIR}:${temp_dir}/static \
-    unity-sds/sounder_sips_l1b_pge:${DOCKER_TAG} \
-    -p input_path ${PGE_IN_DIR} \
-    -p output_path ${PGE_OUT_DIR} \
-    -p data_static_path ${temp_dir}/static $
-    $*
+cwl_path=$(realpath "$(dirname $0)"/../cwl)
+
+if [ -z "$(which cwltool)" ]; then
+    echo "cwltool was not found and required."
+    exit 1
+fi
+
+cwltool \
+    --outdir ${PGE_OUT_DIR} \
+    ${cwl_path}/l1b_package.cwl \
+    --input_dir ${PGE_IN_DIR} \
+    $* |& tee ${PGE_OUT_DIR}/cwltool.log
 
 echo "Output available at: $PGE_OUT_DIR"
